009da4797a5a0ac9ea167325e7002e87a867e705
Andrew Bernier <a6bernier@gmail.com>
2018-09-03 18:19:56 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -9,7 +9,7 @@ def countdown(number)
   return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep()
+def countdown_with_sleep(sleep 5)
   number = 10
   while number > 0
     puts "#{number} SECOND(S)!"
